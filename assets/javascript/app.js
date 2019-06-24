c34453505926c1401e24a3ef7ae31b8d3e8b004c
Stephen Malito <stevemalito@gmail.com>
2019-06-24 13:37:01 -0500

    Updated about me

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,25 @@
 var live = "about";
 // hide()
-
-$("#portfolio").on("click", function () {
-    console.log("clicked")
-
-        $(".about").hide()
-        $(".porto").show()
-
-
-});
-$("#about").on("click", function () {
-    console.log("clicked")
+$(".porto").hide()
+    $(".about").show()
+    
+    $("#portfolio").on("click", function () {
+        console.log("clicked")
+    
+            // $(".about").hide()
+            $(".porto").slideDown()
+    
+    
+    });
+    $("#about").on("click", function () {
+        console.log("clicked")
         $(".porto").hide()
-        $(".about").show()
+        $(".about").slideDown()
+    
+    });
 
 
-});
+
+
 
 
